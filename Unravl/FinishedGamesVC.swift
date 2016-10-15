834503374673ea8888d793056f9059bd0213438e
Jillian Somera <jsomera@gmail.com>
2016-10-14 23:24:23 -0500

    Create finished games vc

---
+++ Unravl/FinishedGamesVC.swift
@@ -0,0 +1,35 @@
+//
+//  FinishedGamesVC.swift
+//  Unravl
+//
+//  Created by Jillian Somera on 10/14/16.
+//  Copyright © 2016 Lit AF. All rights reserved.
+//
+
+import UIKit
+
+class FinishedGamesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
