ff77051fcc0742e55769fabe58e867fab0c48ff2
Jillian Somera <jsomera@gmail.com>
2016-10-20 12:44:46 -0500

    Import tutorial files

---
+++ Unravl/tutorial/js/dropzone_render.js
@@ -0,0 +1,6 @@
+function dropzoneRender() {
+	$('body').html("<div id=\"outer-dropzone\" class=\"dropzone\"><span id=\"sentence\" class=\"float-left\" ></span><div id=\"inner-dropzone\" class=\"dropzone\"></div></div><div id=\"word-text\" class=\"draggable drag-drop yes-drop\"></div>")
+}
+
+
+
